aa538ba2b6421d487f47931754b0db694cf14460
Tony Baker <this.tonybaker@gmail.com>
2019-01-19 20:40:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
 	if speed > 60 || speed < 40
-		"true"
+		true
 	else
-		"false"
+		false
 	end
 end
 
 
 
 def not_safe?(speed)
-	speed > 60 || speed < 40 ? "true" : "false"
+	speed > 60 || speed < 40 ? true : false
 end
